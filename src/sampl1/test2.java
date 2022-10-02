33a683a098f8064f8eb6623b4694ee4a15e0cf41
HP <HP@DESKTOP-NRC>
2022-10-02 18:38:10 +0530

    First Commit1

---
+++ src/sampl1/test2.java
@@ -0,0 +1,14 @@
+package sampl1;
+
+public class test2 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("How r You");
+	  m2();
+	}
+  public static void m2()
+  {
+	  System.out.println("Hello");
+  }
+}
